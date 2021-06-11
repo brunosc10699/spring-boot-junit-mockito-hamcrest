1fda02f84fde58a7f042fe32afbfc8a98404b016
brunosc10699 <brunosc10699@gmail.com>
2021-06-11 14:44:09 -0300

    Project created

---
+++ src/main/java/com/bruno/studentsmanagement/StudentsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.studentsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/studentsmanagement/StudentsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.studentsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
