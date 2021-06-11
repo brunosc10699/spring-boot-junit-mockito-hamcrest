735d7457a29cf6f266bb463b422d33895de547b5
brunosc10699 <brunosc10699@gmail.com>
2021-06-11 15:31:29 -0300

    Student resource ready to receive the endpoints

---
+++ src/main/java/com/bruno/studentsmanagement/resources/StudentResource.java
@@ -0,0 +1,15 @@
+package com.bruno.studentsmanagement.resources;
+
+import com.bruno.studentsmanagement.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/students")
+public class StudentResource {
+
+    @Autowired
+    private StudentService studentService;
+    
+}
